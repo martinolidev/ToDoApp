28b4cb9a2587b716bf774bc5d3e1bb75ee9447c7
Martin Martinez <mtz.martin@proton.me>
2023-11-15 13:40:38 -0600

    Designing the cards for the to-do's

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -28,6 +28,26 @@ struct MainView: View {
     }
     
     var body: some View {
+        titleBar
+        ScrollView {
+            toDoCard
+        }
+    }
+    
+    var toDoCard: some View {
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.indigo)
+                .frame(height: 70)
+            Text("Ejemplo 1")
+                .foregroundStyle(Color(.white))
+                .font(.title)
+                .bold()
+                .padding()
+        }
+    }
+    
+    var titleBar: some View {
         HStack {
             Text("To do")
                 .font(.largeTitle)
@@ -37,7 +57,6 @@ struct MainView: View {
                 .font(.title3)
                 .bold()
         }
-
     }
 }
 
