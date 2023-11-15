203b11dfa9d3dc407abc6c43ed93e6f5c7c8f58c
Martin Martinez <mtz.martin@proton.me>
2023-11-15 13:01:56 -0600

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Martin Martinez on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
