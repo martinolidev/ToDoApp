83234da1a425ec986ef4c17a491128846fe867e3
Martin Martinez <mtz.martin@proton.me>
2023-11-15 13:32:06 -0600

    I added the current date with a function

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -10,12 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MainView()
         }
         .padding()
+    }
+}
+
+struct MainView: View {
+    //A state property to allow dynamic changes on the date
+    @State var currentDate = Date()
+    
+    //Formats currentDate using given date format, returns formatted string
+    func getCurrentDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: currentDate)
+    }
+    
+    var body: some View {
+        HStack {
+            Text("To do")
+                .font(.largeTitle)
+                .bold()
+            Spacer()
+            Text(getCurrentDate())
+                .font(.title3)
+                .bold()
+        }
+
     }
 }
 
